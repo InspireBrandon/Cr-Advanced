7acd56f468f7765255dcc856478b7b6cd7126bd8
Brandon Lange <brandon@devinspire.co.za>
2019-03-08 12:01:37 +0200

    added no apps installed block

--- src/components/Main/Apps/Index.vue
+++ src/components/Main/Apps/Index.vue
@@ -2,11 +2,18 @@
     <div>
         <v-progress-linear v-if="showLoader" class="ma-0" color="primary" indeterminate height="5"></v-progress-linear>
         <v-container v-if="!showLoader" fluid grid-list-md>
-            <v-layout row wrap>
+            <v-layout row wrap v-if="appConfigDetail.length > 0">
                 <v-flex lg3 md4 sm6 xs12 v-for="(appConfigDetail, index) in appConfigDetail" :key="index">
                     <app-block :appConfigDetail="appConfigDetail"></app-block>
                 </v-flex>
-                <v-flex lg4 md4 sm4 xs4>
+            </v-layout>
+            <v-layout row wrap v-else>
+                <v-flex lg12 md12 sm12 xs12 v-if="appConfigDetail.length <= 0">
+                    <v-card>
+                        <v-card-text>
+                            <p>It appears that you dont have any apps installed. <a href="#" @click.prevent="$router.push('/Store')">Click Here</a> to go and install some.</p>
+                        </v-card-text>
+                    </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -14,6 +21,9 @@
 </template>
 
 <script>
+    import Axios from 'axios';
+    import jwt from 'jsonwebtoken';
+
     import ApplicationDetailsHelper from '@/libs/system/application/application-details-helper.js';
     import app_block from '@/components/Main/Apps/app_block.vue';
 
@@ -34,23 +44,48 @@
         },
         created() {
             let self = this;
-            self.applicationDetailsHelper = new ApplicationDetailsHelper();
-            self.apps = self.applicationDetailsHelper.getAllApplications();
-
-            self.apps.forEach(app => {
-                self.appConfigDetail.push(new ConfigDetail(
-                    self.applicationDetailsHelper.getApplicationConfigBySystemCode(app.system_code),
-                    self.applicationDetailsHelper.getApplicationDetailsBySystemCode(app.system_code)
-                ));
-            })
-
-            self.showLoader = false;
+            self.getInstalledApps();
         },
-        methods: {}
+        methods: {
+            getInstalledApps() {
+                let self = this;
+                let encoded_details = jwt.decode(sessionStorage.accessToken);
+
+                Axios.get(process.env.VUE_APP_API + `Features?systemUserID=${encoded_details.USER_ID}`)
+                    .then(r => {
+                        self.getAppsFromManifest(r.data)
+                    })
+            },
+            getAppsFromManifest(installedApps) {
+                let self = this;
+
+                self.applicationDetailsHelper = new ApplicationDetailsHelper();
+                self.apps = self.applicationDetailsHelper.getAllApplications();
+
+                self.apps.forEach(app => {
+                    let appInstalled = false
+
+                    installedApps.forEach(installedApp => {
+                        if (app.system_code == installedApp.systemCode)
+                            appInstalled = true;
+                    })
+
+                    if (appInstalled) {
+                        self.appConfigDetail.push(new ConfigDetail(
+                            self.applicationDetailsHelper.getApplicationConfigBySystemCode(app.system_code),
+                            self.applicationDetailsHelper.getApplicationDetailsBySystemCode(app.system_code)
+                        ));
+                    }
+                })
+
+                self.showLoader = false;
+            }
+        }
     }
 
-    function ConfigDetail(config, detail) {
+    function ConfigDetail(config, detail, system_code) {
         this.config = config;
         this.detail = detail;
+        this.system_code = system_code;
     }
 </script>
