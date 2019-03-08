efc00c2b6a751474e34be855a2c3c55868e7d308
Brandon Lange <brandon@devinspire.co.za>
2019-03-08 09:22:32 +0200

    scrollbar

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,26 @@
 
   .main-main {
     overflow: auto;
-    height: calc(100vh - 60px);
+    height: calc(100vh - 50px);
+  }
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
   }
 </style>
