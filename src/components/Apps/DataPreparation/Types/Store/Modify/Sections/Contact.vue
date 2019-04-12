22c80add63679c2019dc86377ef91048b28045e9
tjVisser <tj.visser@devinspire.co.za>
2019-04-12 11:03:54 +0200

    typo fix

--- src/components/Apps/DataPreparation/Types/Store/Modify/Sections/Contact.vue
+++ src/components/Apps/DataPreparation/Types/Store/Modify/Sections/Contact.vue
@@ -5,7 +5,7 @@
             <v-container grid-list-md>
                 <v-layout row wrap>
                     <v-flex xl6 lg6 md6 sm12 xs12>
-                        <v-text-field v-model="form.store_Manager_Cell" label="Phone"></v-text-field>
+                        <v-text-field v-model="form.store_Phone" label="Phone"></v-text-field>
                     </v-flex>
                     <v-flex xl6 lg6 md6 sm12 xs12>
                         <v-text-field v-model="form.store_Fax"  label="Fax"></v-text-field>
@@ -26,7 +26,7 @@
                 valid: true,
                 form: {
                     
-                    store_Manager_Cell: '',
+                    store_Phone: '',
                     store_Fax: '',
                     store_Email: ''
                 }
