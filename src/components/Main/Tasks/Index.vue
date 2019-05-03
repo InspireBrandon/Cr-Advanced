99aea99ba4086cabdfbbab9516cace43e4fcb084
Brandon Lange <brandon@devinspire.co.za>
2019-05-03 14:49:30 +0200

    busy with tasks view

--- src/components/Main/Tasks/Index.vue
+++ src/components/Main/Tasks/Index.vue
@@ -2,8 +2,36 @@
     <v-container grid-list-md fluid>
         <v-layout row wrap>
             <v-flex md12>
-                <v-card>
-                    asdf
+                <v-card flat>
+                    <v-toolbar flat dark dense color="primary"></v-toolbar>
+                    <v-card-text class="pa-0">
+                        <v-data-table :headers="headers" :items="projectTransactions" class="elevation-0 scrollable"
+                            hide-actions>
+                            <template v-slot:items="props">
+                                <tr :style="{ backgroundColor: (props.item.subtask == true  ? 'lightgrey' : 'transparent' )}">
+                                    <td>{{ props.item.planogram }}</td>
+                                    <td>{{ typeList[props.item.type == -1 ? 5 : props.item.type].text }}</td>
+                                    <td>{{ status[props.item.status == -1 ? 18 : props.item.status].text }}</td>
+                                    <td>{{ props.item.storeCluster }}</td>
+                                    <td>{{ props.item.categoryCluster }}</td>
+                                    <td>{{ props.item.store }}</td>
+                                    <td>{{ props.item.dateTimeString }}</td>
+                                    <td style="width: 5%;">
+                                        <!-- PROJECT START -->
+                                        <v-btn @click="assignFirstTask(props.item)" small v-if="props.item.status == 0">
+                                            Assign</v-btn>
+                                        <!-- END PROJECT START -->
+                                        <!-- DATA PREPARATION -->
+                                        <v-btn small v-if="props.item.type == 1"></v-btn>
+                                        <v-btn small v-if="props.item.type == 1"></v-btn>
+                                        <v-btn small v-if="props.item.type == 1"></v-btn>
+                                        <v-btn small v-if="props.item.type == 1"></v-btn>
+                                        <!-- END DATA PREPARATION -->
+                                    </td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -33,6 +61,39 @@
         },
         data() {
             return {
+                headers: [{
+                        text: 'Project',
+                        sortable: false
+                    },
+                    {
+                        text: 'Type',
+                        sortable: false
+                    },
+                    {
+                        text: 'Status',
+                        sortable: false
+                    },
+                    {
+                        text: 'Store Cluster',
+                        sortable: false
+                    },
+                    {
+                        text: 'Category Cluster',
+                        sortable: false
+                    },
+                    {
+                        text: 'Store',
+                        sortable: false
+                    },
+                    {
+                        text: 'Date',
+                        sortable: false
+                    },
+                    {
+                        text: '',
+                        sortable: false
+                    }
+                ],
                 showLoader: true,
                 accessType: null,
                 projectTransactionsProjectTab: [],
@@ -44,7 +105,39 @@
                 users: []
             }
         },
+        created() {
+            let self = this;
+
+            self.getLists(() => {
+                let encoded_details = jwt.decode(sessionStorage.accessToken);
+                let systemUserID = encoded_details.USER_ID;
+                self.getTransactionsByUser(systemUserID, () => {
+
+                })
+            })
+        },
         methods: {
+            getLists(callback) {
+                let self = this
+                let statusHandler = new StatusHandler()
+                self.status = statusHandler.getStatus()
+                self.typeList = statusHandler.getTypeList()
+                callback()
+            },
+            getTransactionsByUser(systemUserID, callback) {
+                let self = this;
+
+                Axios.defaults.headers.common["TenantID"] = sessionStorage.currentDatabase;
+
+                Axios.get(process.env.VUE_APP_API + `UserProjectTX?userID=${systemUserID}`).then(r => {
+                        self.projectTransactions = r.data.projectTXList;
+                        delete Axios.defaults.headers.common["TenantID"];
+                        callback();
+                    })
+                    .catch(e => {
+                        delete Axios.defaults.headers.common["TenantID"];
+                    })
+            },
         }
     }
 </script>
