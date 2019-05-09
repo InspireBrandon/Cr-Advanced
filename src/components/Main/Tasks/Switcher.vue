e15096bfb63ac4399a91a7f58ae69847a14ef6f8
Brandon Lange <brandon@devinspire.co.za>
2019-05-09 09:48:11 +0200

    Removed comments

--- src/components/Main/Tasks/Switcher.vue
+++ src/components/Main/Tasks/Switcher.vue
@@ -1,97 +1,94 @@
 <template>
-    <v-card>
-        <v-toolbar flat dark dense color="primary">
-            <!-- <v-text-field prepend-inner-icon="search" placeholder="Search" dark></v-text-field> -->
-            <v-autocomplete prepend-inner-icon="search" placeholder="Search"  :items="filterList" v-model="dropSearch">
-            </v-autocomplete>
-            <v-spacer></v-spacer>
-            <v-spacer></v-spacer>
+    <v-container grid-list-md>
+        <v-layout row wrap>
+            <v-flex md12>
+                <v-card>
+                    <v-toolbar flat dark dense color="primary">
+                        <!-- <v-text-field prepend-inner-icon="search" placeholder="Search" dark></v-text-field> -->
+                        <v-autocomplete prepend-inner-icon="search" placeholder="Search" :items="filterList"
+                            v-model="dropSearch">
+                        </v-autocomplete>
+                        <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
 
-            <v-btn-toggle v-model="searchType" class="transparent" multiple>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="1" flat>
-                            <v-icon>
-                                perm_data_setting</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Data-Prep</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="2" flat>
-                            <v-icon>assessment</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Ranging</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="3" flat>
-                            <v-icon>web</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Planogram</span>
-                </v-tooltip>
-            </v-btn-toggle>
-        </v-toolbar>
-        <v-toolbar dense flat dark>
-            <v-autocomplete v-if="selectedView==2" placeholder=" store" :items="users" v-model="selectedUser" @change="changeStore()"></v-autocomplete>
-             <v-autocomplete v-if="selectedView==0" placeholder="users " :items="users" v-model="selectedUser" @change="changeView()"></v-autocomplete>
-            <v-spacer></v-spacer>
-            <v-spacer></v-spacer>
-            <v-btn-toggle v-model="selectedView" class="transparent" >
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="0" flat>
-                            <v-icon>
-                                all_inbox</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Tasks</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="1" flat>
-                            <v-icon>bookmarks</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Projects</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <v-btn v-on="on" :value="2" flat>
-                            <v-icon>account_balance</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Stores</span>
-                </v-tooltip>
-            </v-btn-toggle>
-        </v-toolbar>
-        <!-- <v-container grid-list-md>
-            <v-layout row wrap>
-                <v-flex md4>
-                    <v-select label="Select view" :items="views" v-model="selectedView">
-                    </v-select>
-                </v-flex>
-                <v-flex md8>
-
-                </v-flex>
-
-            </v-layout>
-        </v-container> -->
-
-        <Tasks :searchTypeComp="searchType" :dropSearchComp="dropSearch" v-if="selectedView==0" />
-        <MyProjects :searchTypeComp="searchType" :dropSearchComp="dropSearch" v-if="selectedView==1" />
-        <Planograms ref="Planograms" :searchTypeComp="searchType" :dropSearchComp="dropSearch" v-if="selectedView==2" />
-    </v-card>
+                        <v-btn-toggle v-model="searchType" class="transparent" multiple>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="1" flat>
+                                        <v-icon>
+                                            perm_data_setting</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Data-Prep</span>
+                            </v-tooltip>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="2" flat>
+                                        <v-icon>assessment</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Ranging</span>
+                            </v-tooltip>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="3" flat>
+                                        <v-icon>web</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Planogram</span>
+                            </v-tooltip>
+                        </v-btn-toggle>
+                    </v-toolbar>
+                    <v-toolbar dense flat dark>
+                        <v-autocomplete v-if="selectedView==2" placeholder=" store" :items="users"
+                            v-model="selectedUser" @change="changeStore()"></v-autocomplete>
+                        <v-autocomplete v-if="selectedView==0" placeholder="users " :items="users"
+                            v-model="selectedUser" @change="changeView()"></v-autocomplete>
+                        <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
+                        <v-btn-toggle v-model="selectedView" class="transparent">
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="0" flat>
+                                        <v-icon>
+                                            all_inbox</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Tasks</span>
+                            </v-tooltip>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="1" flat>
+                                        <v-icon>bookmarks</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Projects</span>
+                            </v-tooltip>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn v-on="on" :value="2" flat>
+                                        <v-icon>account_balance</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Stores</span>
+                            </v-tooltip>
+                        </v-btn-toggle>
+                    </v-toolbar>
+                    <Tasks :searchTypeComp="searchType" :dropSearchComp="dropSearch" v-if="selectedView==0" />
+                    <MyProjects :searchTypeComp="searchType" :dropSearchComp="dropSearch" v-if="selectedView==1" />
+                    <Planograms ref="Planograms" :searchTypeComp="searchType" :dropSearchComp="dropSearch"
+                        v-if="selectedView==2" />
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     import Tasks from "./Index.vue"
     import MyProjects from "./MyProjects.vue"
     import Planograms from "./Planograms.vue"
-   import {
+    import {
         EventBus
     } from '@/libs/events/event-bus.js';
 
@@ -103,11 +100,11 @@
         },
         data() {
             return {
-                
-                filterList:[],
-                Search:null,
-                dropSearch:null,
-                searchType:null,
+
+                filterList: [],
+                Search: null,
+                dropSearch: null,
+                searchType: null,
                 selectedUser: null,
                 users: [],
                 selectedView: 0,
@@ -123,18 +120,18 @@
                 }, ]
             }
         },
-        mounted(){
-             let self = this
-            EventBus.$on('filter-items-changed',list => {
-               self.filterList=[]
-               self.filterList = list;
+        mounted() {
+            let self = this
+            EventBus.$on('filter-items-changed', list => {
+                self.filterList = []
+                self.filterList = list;
             });
-             EventBus.$on('stores-items-changed',list => {
-               self.users=[]
+            EventBus.$on('stores-items-changed', list => {
+                self.users = []
                 self.users = list;
             });
         },
-        created(){
+        created() {
             let self = this
             EventBus.$off('filter-items-changed');
         },
@@ -145,13 +142,13 @@
 
                 })
             },
-            changeStore(){
-                 let self = this
+            changeStore() {
+                let self = this
                 self.$nextTick(() => {
-                self.$refs.Planograms.getData(self.selectedUser)
+                    self.$refs.Planograms.getData(self.selectedUser)
                 })
             }
-             
+
         }
 
     }
