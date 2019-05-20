33051da0c59a37a10da3eabb7f4fae8e3c4a23e9
Jason Werber <jasonwerber@gmail.com>
2019-05-20 16:25:21 +0200

    Playing around with floor planning ideas

---
+++ src/components/Main/FloorPlanning/BaseLibs/Scene/Playground/product.js
@@ -0,0 +1,29 @@
+import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+
+class Product {
+    constructor (world) {
+        this.world = world;
+    }
+
+    addBox() {
+        let self = this;
+        var myMaterial = new BABYLON.StandardMaterial("myMaterial", this.world.scene);
+        let url = localStorage.ServerAddress + 'ProductImage?rng=' + Math.random() + '&folder=000000018&file=82223.1&compress=true'
+        myMaterial.diffuseTexture = new BABYLON.Texture(url, this.world.scene);
+
+        var myBox = BABYLON.MeshBuilder.CreateBox("myBox", {height: 5, width: 5, depth: 2}, self.world.scene);
+        myBox.position.x  =  Math.random() * 100;
+        myBox.position.y  =  Math.random() * 100;
+        myBox.position.z  =  Math.random() * 100;
+        myBox.material = myMaterial;
+    }
+
+    addLots() {
+        let self = this;
+        for (var i = 0 ; i < 10; i ++) {
+            self.addBox();
+        }
+    }
+}
+
+export default Product;
