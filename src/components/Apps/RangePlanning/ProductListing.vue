2476e59fc12915f9f785afe5142285df60fcb617
Brandon Lange <brandon@devinspire.co.za>
2019-05-24 10:30:30 +0200

    added tmp logic for app view

--- src/components/Apps/RangePlanning/ProductListing.vue
+++ src/components/Apps/RangePlanning/ProductListing.vue
@@ -1,3 +1,20 @@
 <template>
     
 </template>
+
+<script>
+export default {
+    name: 'product-listing',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        show() {
+            let self = this;
+        }  
+    }
+}
+</script>
+
