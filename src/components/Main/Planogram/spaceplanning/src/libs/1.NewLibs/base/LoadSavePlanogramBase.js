74d73febf5ffd66432c5f6e4e5f1c1afd30a4cbc
tjVisser <tj.visser@devinspire.co.za>
2019-07-01 11:44:14 +0200

    added fixture type to space planning

--- src/components/Main/Planogram/spaceplanning/src/libs/1.NewLibs/base/LoadSavePlanogramBase.js
+++ src/components/Main/Planogram/spaceplanning/src/libs/1.NewLibs/base/LoadSavePlanogramBase.js
@@ -97,6 +97,10 @@ class LoadSavePlanogramBase {
         planogramName += " - S" + dimensionData.supplierStands;
         planogramName += " - B" + dimensionData.bins;
       }
+      
+      if (planogramName != "") {
+        planogramName += " - " + dimensionData.FixtureType;
+      }
     } else {
       planogramName = spacePlanName;
     }
