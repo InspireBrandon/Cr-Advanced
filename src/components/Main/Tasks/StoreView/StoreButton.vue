5348582a827db94d47d60726bfbcf18787f92b7f
tjVisser <tj.visser@devinspire.co.za>
2019-07-11 13:07:16 +0200

    store planogram Button changes

--- src/components/Main/Tasks/StoreView/StoreButton.vue
+++ src/components/Main/Tasks/StoreView/StoreButton.vue
@@ -45,7 +45,7 @@
                 <span>View</span>
             </v-tooltip>
         </div>
-        <div v-if="params.data.planogramStoreStatus!=0&&params.data.planogramStoreStatus!=6&&params.data.planogramStoreStatus!=7">
+        <div v-if="params.data.planogramStoreStatus!=0&&params.data.planogramStoreStatus!=4&&params.data.planogramStoreStatus!=6&&params.data.planogramStoreStatus!=7">
             <v-tooltip bottom>
                 <template v-slot:activator="{ on }">
                     <v-btn v-on="on" @click="params.context.componentParent.openOrder(params,0,'Store Variation')" icon flat small
@@ -56,7 +56,7 @@
                 <span>request Store variation</span>
             </v-tooltip>
         </div>
-          <div v-if="params.data.planogramStoreStatus!=0&&params.data.planogramStoreStatus!=6&&params.data.planogramStoreStatus!=7">
+          <div v-if="params.data.planogramStoreStatus!=0&&params.data.planogramStoreStatus!=4&&params.data.planogramStoreStatus!=6&&params.data.planogramStoreStatus!=7">
             <v-tooltip bottom>
                 <template v-slot:activator="{ on }">
                     <v-btn v-on="on" @click="params.context.componentParent.openOrder(params,1,'Model Variation')" icon flat small
