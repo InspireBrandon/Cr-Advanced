3594ee3332507c89cdc8bfc1b981ae518b4a91b1
Brandon Lange <brandon@devinspire.co.za>
2019-07-18 09:46:08 +0200

    busy with project view

---
+++ src/components/Main/Tasks/ProjectView/GridComponents/DataPrepAction.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <!-- <v-btn color="success" icon flat small>
+            <v-icon>play_arrow</v-icon>
+        </v-btn> -->
+        <div class="btn_grid start">
+            <div class="btn_text">Start</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .start {
+        background: #ff7200;
+        border: 1px solid grey;
+        color: white;
+    }
+
+    .btn_grid {
+        height: 20px;
+        border-radius: 3px;
+        margin-top: 8px;
+        text-align: center;
+        font-size: 18px;
+    }
+
+    .start:hover {
+        background: #c75a00;
+        cursor: pointer;
+    }
+
+    .btn_text {
+        margin-top: -5px;
+    }
+</style>
---
+++ src/components/Main/Tasks/ProjectView/GridComponents/RangeAction.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <!-- <v-btn color="success" icon flat small>
+            <v-icon>play_arrow</v-icon>
+        </v-btn> -->
+        <v-btn color="warning" icon flat small>
+            <v-icon>play_circle_outline</v-icon>
+        </v-btn>
+    </div>
+</template>
