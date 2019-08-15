06c980ac71652c7c766f92d3b3b10cb12a325175
tjVisser <tj.visser@devinspire.co.za>
2019-08-15 13:31:44 +0200

    addded maint modal and grid

---
+++ src/components/Apps/Strategy/BasketAnalysis/basketMaint/BasketMaintenanceModal.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-dialog v-model="dailog" persistent fullscreen>
+        <v-card>
+            <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>
+                    Basket Maintenance
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon @click="dialog=false">
+                    <v-icon>
+                        close
+                    </v-icon>
+                </v-btn>
+            </v-toolbar>
+        </v-card>
+
+
+        <grid ref="grid"/>
+    </v-dialog>
+</template>
+<script>
+    import grid from "./Grid.vue"
+    export default {
+        components: {
+            grid
+        },
+        data() {
+            return {
+                dialog: false,
+                callback: null,
+            }
+        },
+        methods: {
+            open(callback) {
+                let self = this
+                self.callback = callback
+            },
+            submit() {},
+
+        }
+
+    }
+</script>
