eb0c38ca4a37b95c9e9bec4d39cec5f584ccd639
tjVisser <tj.visser@devinspire.co.za>
2019-08-22 15:34:33 +0200

    chnages for progreess liner

--- src/components/Apps/Strategy/ListingClusters/progressRenderer.vue
+++ src/components/Apps/Strategy/ListingClusters/progressRenderer.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <!-- <v-progress-linear v-model="params.data.percent"> -->
-        <v-progress-linear :value="20">
-            
-            </v-progress-linear>
+        <v-progress-linear :value="params.data.cumulativProductSales">
+
+        </v-progress-linear>
 
 
     </div>
