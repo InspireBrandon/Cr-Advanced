8f8d5c675a0798835c70acd0db97535bbb6b5c12
tjVisser <tj.visser@devinspire.co.za>
2019-09-16 08:39:37 +0200

    busy adding switch for maps

--- src/components/Apps/Strategy/Map/Index.vue
+++ src/components/Apps/Strategy/Map/Index.vue
@@ -7,7 +7,8 @@
     import * as am4charts from "@amcharts/amcharts4/charts";
     import * as am4maps from "@amcharts/amcharts4/maps";
     import am4geodata_worldLow from "@amcharts/amcharts4-geodata/southAfricaHigh";
-    import am4themes_themes from "@amcharts/amcharts4/themes/themes/dark.js";
+
+
 
     export default {
         props: ["rowData"],
@@ -17,12 +18,11 @@
             }
         },
         mounted() {
-            this.drawMap(this.labels)
+            this.drawMap(this.labels, 0)
         },
         methods: {
-            drawMap(state) {
+            drawMap(labelState, type) {
                 let self = this
-                am4core.useTheme(am4themes_themes);
                 console.log(self.rowData);
 
                 let formattedData = [];
@@ -55,27 +55,75 @@
                 // define template
                 imageSeries.data = formattedData
                 imageSeries.dataFields.value = "sales";
+                if (type == 0) {
+                    let imageSeriesTemplate = imageSeries.mapImages.template;
+                    imageSeriesTemplate.propertyFields.latitude = "lat";
+                    imageSeriesTemplate.propertyFields.longitude = "long";
+                    imageSeriesTemplate.nonScaling = true
 
-                let imageSeriesTemplate = imageSeries.mapImages.template;
-                imageSeriesTemplate.propertyFields.latitude = "lat";
-                imageSeriesTemplate.propertyFields.longitude = "long";
-                imageSeriesTemplate.nonScaling = true
-
-                var circle = imageSeriesTemplate.createChild(am4core.Circle);
-                circle.fillOpacity = 0.7;
-                circle.propertyFields.fill = 'color';
-                // circle.fill=am4core.color("#fff");
-                circle.tooltipText = "{storeName}: [bold]{sales}[/]";
-
-
+                    var circle = imageSeriesTemplate.createChild(am4core.Circle);
+                    circle.fillOpacity = 0.7;
+                    circle.propertyFields.fill = 'color';
+                    // circle.fill=am4core.color("#fff");
+                    circle.tooltipText = "{storeName}: [bold]{sales}[/]";
 
 
-                let label = imageSeriesTemplate.createChild(am4core.Label);
-                label.text = "{storeName}";
-                label.fill = am4core.color("#fff");
 
-                label.nonScaling = false;
-                label.hidden = state
+
+                    let label = imageSeriesTemplate.createChild(am4core.Label);
+                    label.text = "{storeName}";
+                    label.fill = am4core.color("#fff");
+
+                    label.nonScaling = false;
+                    label.hidden = labelState
+
+                    imageSeries.heatRules.push({
+                        "target": circle,
+                        "property": "radius",
+                        "min": 4,
+                        "max": 30,
+                        "dataField": "value"
+                    })
+                }
+                if (type == 1) { // start pie chart series
+                    let pieSeries = chart.series.push(new am4maps.MapImageSeries());
+                    let pieTemplate = pieSeries.mapImages.template;
+                    pieTemplate.propertyFields.latitude = "lat";
+                    pieTemplate.propertyFields.longitude = "long";
+                    pieTemplate.nonScaling = true
+                    let pieChartTemplate = pieTemplate.createChild(am4charts.PieChart);
+                    pieChartTemplate.adapter.add("data", function (data, target) {
+                        if (target.dataItem) {
+                            return target.dataItem.dataContext.pieData;
+                        } else {
+                            return [];
+                        }
+                    });
+                    pieChartTemplate.propertyFields.width = "width";
+                    pieChartTemplate.propertyFields.height = "height";
+                    pieChartTemplate.horizontalCenter = "middle";
+                    pieChartTemplate.verticalCenter = "middle";
+
+                    // let pieTitle = pieChartTemplate.titles.create();
+                    // pieTitle.text = "{storeName}";
+
+                    let pieSeriesTemplate = pieChartTemplate.series.push(new am4charts.PieSeries);
+                    pieSeriesTemplate.dataFields.category = "category";
+                    pieSeriesTemplate.dataFields.value = "value";
+                    pieSeriesTemplate.labels.template.disabled = true;
+                    // pieSeriesTemplate.ticks.template.disabled = true;
+                    pieSeries.nonScalingStroke = false
+
+
+                    let graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+                    graticuleSeries.mapLines.template.line.strokeOpacity = 0.2;
+                    graticuleSeries.longitudeStep = 2;
+                    graticuleSeries.latitudeStep = 2
+
+
+                    pieSeries.data = self.rowData
+                }
+
 
                 let button = chart.chartContainer.createChild(am4core.Button);
                 button.label.text = "toggle labels";
@@ -88,122 +136,6 @@
                     self.drawMap(self.labels)
                 });
 
-                imageSeries.heatRules.push({
-                    "target": circle,
-                    "property": "radius",
-                    "min": 4,
-                    "max": 30,
-                    "dataField": "value"
-                })
-
-                // start pie chart series
-                let pieSeries = chart.series.push(new am4maps.MapImageSeries());
-                let pieTemplate = pieSeries.mapImages.template;
-                pieTemplate.propertyFields.latitude = "lat";
-                pieTemplate.propertyFields.longitude = "long";
-                pieTemplate.nonScaling = true
-                let pieChartTemplate = pieTemplate.createChild(am4charts.PieChart);
-                pieChartTemplate.adapter.add("data", function (data, target) {
-                    if (target.dataItem) {
-                        return target.dataItem.dataContext.pieData;
-                    } else {
-                        return [];
-                    }
-                });
-                pieChartTemplate.propertyFields.width = "width";
-                pieChartTemplate.propertyFields.height = "height";
-                pieChartTemplate.horizontalCenter = "middle";
-                pieChartTemplate.verticalCenter = "middle";
-
-                // let pieTitle = pieChartTemplate.titles.create();
-                // pieTitle.text = "{storeName}";
-
-                let pieSeriesTemplate = pieChartTemplate.series.push(new am4charts.PieSeries);
-                pieSeriesTemplate.dataFields.category = "category";
-                pieSeriesTemplate.dataFields.value = "value";
-                pieSeriesTemplate.labels.template.disabled = true;
-                // pieSeriesTemplate.ticks.template.disabled = true;
-                pieSeries.nonScalingStroke=false
-
-
-
-                pieSeries.data = self.rowData
-                
-                
-                // [{
-                //     "title": "North America",
-                //     "latitude": 39.563353,
-                //     "longitude": -99.316406,
-                //     "width": 100,
-                //     "height": 100,
-                //     "pieData": [{
-                //         "category": "Category #1",
-                //         "value": 1200
-                //     }, {
-                //         "category": "Category #2",
-                //         "value": 500
-                //     }, {
-                //         "category": "Category #3",
-                //         "value": 765
-                //     }, {
-                //         "category": "Category #4",
-                //         "value": 260
-                //     }]
-                // }, {
-                //     "title": "Europe",
-                //     "latitude": 50.896104,
-                //     "longitude": 19.160156,
-                //     "width": 50,
-                //     "height": 50,
-                //     "pieData": [{
-                //         "category": "Category #1",
-                //         "value": 200
-                //     }, {
-                //         "category": "Category #2",
-                //         "value": 600
-                //     }, {
-                //         "category": "Category #3",
-                //         "value": 350
-                //     }]
-                // }, {
-                //     "title": "Asia",
-                //     "latitude": 47.212106,
-                //     "longitude": 103.183594,
-                //     "width": 80,
-                //     "height": 80,
-                //     "pieData": [{
-                //         "category": "Category #1",
-                //         "value": 352
-                //     }, {
-                //         "category": "Category #2",
-                //         "value": 266
-                //     }, {
-                //         "category": "Category #3",
-                //         "value": 512
-                //     }, {
-                //         "category": "Category #4",
-                //         "value": 199
-                //     }]
-                // }, {
-                //     "title": "Africa",
-                //     "latitude": 11.081385,
-                //     "longitude": 21.621094,
-                //     "width": 50,
-                //     "height": 50,
-                //     "pieData": [{
-                //         "category": "Category #1",
-                //         "value": 200
-                //     }, {
-                //         "category": "Category #2",
-                //         "value": 300
-                //     }, {
-                //         "category": "Category #3",
-                //         "value": 599
-                //     }, {
-                //         "category": "Category #4",
-                //         "value": 512
-                //     }]
-                // }]
             }
         }
     }
