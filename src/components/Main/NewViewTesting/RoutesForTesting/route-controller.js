0746d9185c9fcc6c1ca60422e60b7d2126a6e79c
Brandon Lange <brandon@devinspire.co.za>
2019-10-25 11:26:37 +0200

    added routing controller

--- src/components/Main/NewViewTesting/RoutesForTesting/route-controller.js
+++ src/components/Main/NewViewTesting/RoutesForTesting/route-controller.js
@@ -1,11 +1,81 @@
+import RouteItem from './route-item';
+
 class RouteController {
-    constructor() {
-
+    constructor(data) {
+        let self = this;
+        self.userType = data.userType;
+        self.accessRights = data.accessRights;
+        self.routes = GetAllRoutes();
     }
-}
 
-class RouteItem {
-    constructor() {
+    getRoutesByParentID(parentID) {
+        let self = this;
+
+        return self.routes.filter(route => {
+            return route.parentID === parentID && route.allowedAccessLevels.includes(self.userType);
+        })
+    }
+
+    addRoute(routeItem) {
+        let self = this;
         
+        let checkRouteExists = self.routes.find(route => {
+            return route.id == routeItem.id;
+        });
+
+        if(checkRouteExists === undefined || checkRouteExists === null) {
+            self.routes.push(routeItem);
+        }
     }
 }
+
+const accessTypes = {
+    SuperUser: 0,
+    Operations: 1,
+    Buyer: 2,
+    Supplier: 3,
+    Store: 4,
+}
+
+function GetAllRoutes() {
+    let routes = [];
+
+    // Planogram
+    routes.push(
+        new RouteItem({
+            id: "1",
+            parentID: "0",
+            title: "Planogram",
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "1.1",
+            parentID: "1",
+            title: "View",
+            route: "/PlanogramImplementation",
+            allowedAccessLevels: [
+                accessTypes.SuperUser
+            ]
+        }),
+        new RouteItem({
+            id: "1.2",
+            parentID: "1",
+            title: "View",
+            route: "/PlanogramImplementation",
+            allowedAccessLevels: [
+                accessTypes.Supplier,
+                accessTypes.Buyer
+            ]
+        })
+    );
+
+    // Ranging
+    return routes;
+}
+
+export default RouteController;
