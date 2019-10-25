bd65375e52cd818f6b52040a584e301df2a15c41
Douglas Nel <douglas.nel@devinspire.co.za>
2019-10-25 15:05:40 +0200

    getting there

--- src/components/Main/NewViewTesting/RoutesForTesting/route-controller.js
+++ src/components/Main/NewViewTesting/RoutesForTesting/route-controller.js
@@ -12,18 +12,18 @@ class RouteController {
         let self = this;
 
         return self.routes.filter(route => {
-            return route.parentID === parentID && route.allowedAccessLevels.includes(self.userType);
+            return route.parentID == parentID && route.allowedAccessLevels.includes(self.userType);
         })
     }
 
     addRoute(routeItem) {
         let self = this;
-        
+
         let checkRouteExists = self.routes.find(route => {
             return route.id == routeItem.id;
         });
 
-        if(checkRouteExists === undefined || checkRouteExists === null) {
+        if (checkRouteExists === undefined || checkRouteExists === null) {
             self.routes.push(routeItem);
         }
     }
@@ -66,12 +66,111 @@ function GetAllRoutes() {
             id: "1.2",
             parentID: "1",
             title: "View",
-            route: "/PlanogramImplementation",
             allowedAccessLevels: [
                 accessTypes.Supplier,
                 accessTypes.Buyer
             ]
-        })
+        }),
+        new RouteItem({
+            id: "1.3",
+            parentID: "1",
+            title: "Space Planning",
+            route: "/SpacePlanning",
+            allowedAccessLevels: [
+                accessTypes.SuperUser
+            ]
+        }),
+        new RouteItem({
+            id: "1.4",
+            parentID: "1",
+            title: "Default Fixtures",
+            route: "/Fixtures",
+            allowedAccessLevels: [
+                accessTypes.SuperUser
+            ]
+        }),
+        new RouteItem({
+            id: "2",
+            parentID: "0",
+            title: "Ranging",
+            route: "/RangePlanning/:rangeFileID",
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3",
+            parentID: "0",
+            title: "Data Preparation",
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3.1",
+            parentID: "3",
+            title: "Product",
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3.1.1",
+            parentID: "3.1",
+            title: "Brand",
+            route: '/DataPreparation/Brand',
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3.1.2",
+            parentID: "3.1",
+            title: "Manufacturer",
+            route: '/DataPreparation/Manufacturer',
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3.1.3",
+            parentID: "3.1",
+            title: "Supplier",
+            route: '/DataPreparation/Supplier',
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
+        new RouteItem({
+            id: "3.1.4",
+            parentID: "3.1",
+            title: "Department",
+            route: '/DataPreparation/Department',
+            allowedAccessLevels: [
+                accessTypes.SuperUser,
+                accessTypes.Buyer,
+                accessTypes.Supplier,
+                accessTypes.Store
+            ],
+        }),
     );
 
     // Ranging
