1a9941850621d4ee0d8e1735e0c383d2904795c6
Brandon Lange <brandon@devinspire.co.za>
2019-12-05 12:20:28 +0200

    busy with reports

--- src/components/Main/Bi.vue
+++ src/components/Main/Bi.vue
@@ -2,38 +2,112 @@
     <div>
         <v-toolbar dark flat dense>
             <v-toolbar-items>
-                <v-select label="Store Numbers" @change="applyReportFilters" multiple :items="storeNumbers"
-                    v-model="selectedStoreNumbers"></v-select>
+                <v-select style="width: 300px;" label="Reports" @change="choseReport" :items="reports"
+                    v-model="selectedReport"></v-select>
+                <v-select style="width: 300px;" class="ml-3" label="Page" @change="chooseActivePage" :items="pages"
+                    v-model="selectedPage"></v-select>
+                <v-select style="width: 300px;" class="ml-3" label="Store Numbers" multiple @change="applyReportFilters"
+                    :items="storeNumbers" v-model="selectedStoreNumbers"></v-select>
             </v-toolbar-items>
+            <v-btn color="primary" @click="printReport">Print</v-btn>
+            <!-- <v-btn color="primary" @click="switchMode">{{ currentMode == "edit" ? "View" : "Edit" }}</v-btn> -->
         </v-toolbar>
-        <!-- <iframe width="100%" style="height: calc(100vh - 65px);" src="https://app.powerbi.com/view?r=eyJrIjoiMTg0Y2ZjNDQtNjJmMS00MDFhLWI5MzctMDI0MDljNDZkOTI3IiwidCI6ImY2NzU3NzFmLWQ3ZWItNDgxNC04OTkzLWI5MDg2OTcwMTQ1ZSJ9" frameborder="0" allowFullScreen="true"></iframe> -->
         <div ref="reportContainer" id="reportContainer" style="width: 100%; height: calc(100vh - 128px);"></div>
+        <Spinner ref="Spinner" />
     </div>
 </template>
 
 <script>
+    import Axios from 'axios';
     import * as pbi from 'powerbi-client';
     import * as models from 'powerbi-models';
 
-    import Axios from 'axios';
-
-    const accessToken =
-        'H4sIAAAAAAAEACWWtc7GDHaE7-VvHclMkbYwM7M7M9NrdpR7z5fdfoqjRzNz5n_-sbN3XLLyn__-B_XMAoifTtgD0Oj4rYsyQIztIgjrNaHV0HWxjquJqg0Hf_GLQbTZWQTNEN22WpsK_JmlcI64zjISYWyAdVAQNpbcl0M6IfkZI69jq10sp2Ql9rYUdvpJhRP4ZODgTkYCGaBYkwH046JhZwvBV2Og0hzj5NJI1eiDxZEHCA8Pp-DukQxLxbnoq394JeqgZOtaBd0J-OQywkPEzLssFamqGkKReSJG3TjfYRNTM2ABwuyjd5ScL28YHnQxi6HIMTqaoAbjYJYekK2ZMuqrRPvjy3gtN0c6gKHQoOpuk7ZVIZSNH136kuBFs108mqm-aiBGpAjQzoChxdu9-cFe0R4KbTLsSceZJ38OGg_REAR238F7hJ7P2KEi0lGtH02XucJ0PXgH5Zy0r6wTjW9QGAePxx31kMXqjwqwxWsfYZPDvT7bcjqoI9s8ZUbyM6XU5YIIfctuULVcZKMz54cdtMnbLy09dmdXoZcGqaXJYChnt5kqu04EHv__b5GF6kPyw4WQJb3DZ9EEryTy5QnjpdsO94KlPhq0doPibGLh-asRRKoIuoCJ88fH1gK_wtklK6Dd8i-tFVI6U0UB0S8TOKW5MpDwXyVq-p6kNHxd1wueAmP5skXz7DMNUFWS8kMdP0WhwqvpaKMN1cpgLOTq7Lz-dYBdYb5m8M6oqSnqcIZVIJDkichAF-so49DYE_RRFp8Lv10stavLw-Pehjm1WJDATehH5SIIVDCJOmyD4T3RnfB09O-v2DPj6Q5kvZMIKp72pc6g78PUiMYP5ABfnHfW7o2AaX3rvnElvW7qd3cP59vuz_6gUDMDWSgQLU-FFqNBhrbz0bC29qwucV_3Fz7OkWNHHwKMWEvpPsfmcRKyzJeoUNE-GkzPdd8oH87BrjCFosuVqRo16IfAWJC1Qr6xCGiNwZawi73e6oegWwbfUIqXTAGZ37fA00u_h-4uid9k1FMoAlXHs1jA7ehhy2r6u3Gwhb1hsie_wB3Z6vy6wD0H4fuLEKUYrKVSpZm8p0mBY8B-YINvBHDhAO8AKHH8gX5o--BALQa5pE07I09usCLaWqBs8G7-KLETceSvjbpUanjJnUyFCPlApKoCF-Jl7vYqXuCerO-4PyUq4PBsmA83HPov546oU23OM7Gcyvz7UNoUXNQQ6lFUAXfYyppelgusliTL_VozJWvRi2e8dpsNEN7ukmnuQJQvGsv9VgTDk0862nyErzGbee5x4fp4zKwN0aSWNd0rVMZjh_3Nne5DT_SualY6sHwYDEzpRuaK9bIPw4wfJu4mDuxnGpJZsKGVfsoO7jJ3bGuBca3YYVGmrm7pb8eEDs7UWM5wqnc3rVE2MsUOOdQsYYXjZBVuIrGLCRZzkIzkwFeNek5Tr8On7Vac3YqQFWdVMJlIowBaxvAeRFHTrhxUcynNqBxRbWDtpHQpWBrsv3ajGg239KWEm6lYqEtcj1n-3NtxDIWWQYxd6yCcXLOyDU8AST_Yn0-mopEHWrY1-5tmCE9A-PGslYP0YMsXn28sxzOEqDHjfHrBV7KjOuTw5vp1S6gl2ebVSyolRkT9dGtNZMVUA0Mit5acpguA2sTEGxGPuPZLIdJ0kTuHgBPzg2AAumqq4ceLjGYZt69_hUL7Ck_aKqc6fwrxgMRDvp0lbRbS2fGb38CQzO7BWDxrZZO7fMYHIr0bmarJ1Y7J2RbY6lNUhz285b_KkJrrMvmPsStBHL12ZdeOvNZc1eZMtU2LKfrE-EKItDceKila1iL5DWidqxJf1ZP2J9JVjkItU_HAR-3IyMoWEQUgNHvJzQvTg2_BO5mq8eVAC8VTerM6C1tPY7Go3Y3MYxMFHNeL5OLBHwxZpx7XEln92sGRYxp4u7Sq8GrbJOrytYizqmu5nTy7gtjNUDH3MbgzwuDb8yf9-026jgaH5PUvnJPHMSdC3f1-OAMBz3ipiJUN0KojCs1vtNpsJ0MS53vT_m1vaTFnSBA1jvWevprfGgCukvZmaf1nuFTa3dwLb5MvUTOmhjaTT0iZy7h767vyb-rCpPdWoTWeme705mQqR9_zu9k4CwwO8E0M6eup_VKjNfhuWLFRO51VUsGR2k2Z7Xs_vfwHA_oNVUt8aL7hgb3QAojxSi7ZDyaHBAMyEi_JAmP1lEUb-ziiw5Eka1Bn2NV9oPe56adi0tZFqTIqiVdD6LRDhVPzdDoxgtXZcarjMUPf-8TwOV2TbUDxr3_-6x_u967HolXv33TBqMPemGIuQO_AJo8PILlzVsx7IUab1c8m13o2vgRXidV0lMZJ1OpJY-GvTvdbn88GtUCgXp-xqkXmLwjqh7FGFDkJgYSwxwhNowAT1EhdW1z-QEWWmXY-4G0RBRnGe52rZ7iEDYkQEPHgyPy2M9sHvDUP9ZaJtKvjve4xIM2AQ4Njv4qbfJqS9aJC-VF9JNh9QmLTITEB7EUPV9gBRy2Svx9v-2ET6FUdrXbzMh4rO5jUy6zg9ILchgxWEQD6Le1fkuU_34IbrPPqQJbiROm7oJBeCXbXSPPoch-4xI8i_EqOoOaczbKE0BeK1LSrsxumkC2kAlJ1jnyk71duziYqKTj_-jfmd22rnxL-UfYcNHs83RD2E4xQjrq125b-o_K6Zs6O81f9yaR1E-TkQkPhrZa1O97BSldHVVxLc70oiSUiw5QOM5ytQKG4_1moAEJjxLgLakEDz7ZNVrCvn_OLuhA5qRM1XvIcjvDVLdpClRoLwJUjP933IogF28h_OTsmT5ivSiXxADfsUQh2jps9zDmcn9YGGrY7zn7jqoi_aRIJhVse8-KJ9AtsnOXzXT35vfbsPQPomqI7-Y0xN7URWsmw7mNpdD9fqxGUFtsnj6SPQDgsmf-tfSEywFvLhPwRLjpjcXAQ6cfbjo0iOjnLGR_IXhV7NyL7KSjBBi1vbLii52CKg74A-5pgExglelE8Dgmim5p1TuO-jwXM6WFokYrp0uFz-pEyf5j_9_8A012MDK4LAAA='
+    import Spinner from '@/components/Common/Spinner';
 
     export default {
+        components: {
+            Spinner
+        },  
         data() {
             return {
                 report: null,
                 storeNumbers: [1, 2, 3, 4, 5, 6, 7],
-                selectedStoreNumbers: []
+                selectedStoreNumbers: [],
+                reports: [],
+                selectedReport: null,
+                pages: [],
+                selectedPage: null,
+                currentMode: "view"
             }
         },
         mounted() {
             let self = this;
-            self.generateReport();
+
+            self.$refs.Spinner.show();
+
+            self.getReports(() => {
+                self.getReportEmbedConfig();
+            });
         },
         methods: {
-            generateReport() {
+            switchMode() {
+                let self = this;
+
+                if(self.currentMode == "edit") 
+                    self.currentMode = "view"
+                else 
+                    self.currentMode = "edit"
+
+                self.report.switchMode(self.currentMode)
+            },
+            getActivePages() {
+                let self = this;
+
+                self.pages = [];
+
+                self.report.getPages()
+                    .then(pages => {
+                        pages.forEach(page => {
+                            self.pages.push({
+                                value: page.name,
+                                text: page.displayName
+                            })
+                        })
+
+                        if (self.pages.length > 0)
+                            self.selectedPage = self.pages[0].value;
+                    });
+            },
+            getReports(callback) {
+                let self = this;
+
+                Axios.get(process.env.VUE_APP_API + "PowerBI/GetReports")
+                    .then(r => {
+                        self.reports = [];
+
+                        r.data.value.forEach(report => {
+                            self.reports.push({
+                                text: report.name,
+                                value: report.id
+                            })
+                        });
+
+                        self.selectedReport = self.reports[0].value;
+                        callback();
+                    })
+            },
+            getReportEmbedConfig() {
+                let self = this;
+
+                self.$refs.Spinner.show();
+
+                Axios.get(process.env.VUE_APP_API +
+                        `PowerBI/GetReportEmbedConfig?reportID=${self.selectedReport}`)
+                    .then(r => {
+                        self.generateReport(r.data);
+                    })
+            },
+            generateReport(reportEmbedConfig) {
                 let self = this;
 
                 // const basicFilter = {
@@ -50,13 +124,14 @@
 
                 var config = {
                     type: 'report',
-                    id: '67b11930-d835-486a-a6aa-9134479b6696',
-                    embedUrl: 'https://app.powerbi.com/reportEmbed',
+                    id: reportEmbedConfig.id,
+                    embedUrl: reportEmbedConfig.embedUrl, // 'https://app.powerbi.com/reportEmbed',
                     tokenType: 1,
-                    accessToken: accessToken,
+                    accessToken: reportEmbedConfig.embedToken.token,
                     settings: {
                         filterPaneEnabled: false,
-                        navContentPaneEnabled: false
+                        navContentPaneEnabled: false,
+                        background: models.BackgroundType.Transparent
                     }
                 }
 
@@ -65,6 +140,9 @@
 
                 self.report.on('loaded', function (event) {
                     self.getFilters();
+                    self.getActivePages();
+
+                    self.$refs.Spinner.hide();
                 })
 
                 self.getFilters();
@@ -72,13 +150,8 @@
             getFilters() {
                 let self = this;
 
-                console.log("GETTING FILTERS");
-
                 self.report.getFilters()
-                    .then(filters => {
-                        console.log("GOT FILTERS");
-                        console.log(filters);
-                    })
+                    .then(filters => {})
             },
             applyReportFilters() {
                 let self = this;
@@ -88,7 +161,7 @@
                         self.report.removeFilters();
                     } else {
                         const basicFilter = {
-                            $schema: "http://powerbi.com/product/schema#relativeDate",
+                            $schema: "http://powerbi.com/product/schema#basic",
                             target: {
                                 table: "Store",
                                 column: "StoreNumber"
@@ -105,6 +178,29 @@
                             });
                     }
                 })
+            },
+            choseReport() {
+                let self = this;
+
+                self.$nextTick(() => {
+                    self.getReportEmbedConfig();
+                })
+            },
+            chooseActivePage() {
+                let self = this;
+
+                self.$nextTick(() => {
+                    let page = self.report.page(self.selectedPage);
+                    page.setActive();
+                })
+            },
+            printReport() {
+                let self = this;
+
+                self.report.print()
+                    .catch(error => {
+                        console.log("Print error: ", error);
+                    });
             }
         }
     }
---
+++ src/components/Main/SystemFiles/Index.vue
@@ -0,0 +1,155 @@
+<template>
+    <div style="margin-top: 3px;">
+        <div @contextmenu.prevent="onContextMenu" class="route-item" @click="folderClick">
+            <v-icon size="12">{{ showFolders ? 'folder_open' : 'folder' }}</v-icon>
+            <span class="ml-1">Reports</span>
+        </div>
+        <div style="margin-left: 15px;" v-if="showFolders">
+            <SystemFiles :systemUserID="systemUserID" :items="folderFiles" />
+            <div v-if="showAddFolder">
+                <v-form @submit.prevent="submitFolderFile">
+                    <v-icon size="12">{{ currentType == 'folder' ? 'folder' : 'insert_drive_file' }}</v-icon>
+                    <input @blur="showAddFolder = false" ref="folderName" v-model="folderName" class="ml-1"
+                        style="border: 1px solid lightgrey" type="text">
+                </v-form>
+            </div>
+        </div>
+        <v-menu v-model="showMenu" :position-x="x" :position-y="y" absolute offset-y>
+            <v-list dense class="pa-0">
+                <v-list-tile class="pa-0" @click="addFolderFile('folder')">
+                    <v-list-tile-title class="pa-0">Add Folder</v-list-tile-title>
+                </v-list-tile>
+                <v-divider></v-divider>
+                <v-list-tile class="pa-0" @click="addFolderFile('page')">
+                    <v-list-tile-title class="pa-0">Add Page</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-menu>
+    </div>
+</template>
+
+<script>
+    import Axios from 'axios'
+    import jwt from 'jsonwebtoken';
+    import SystemFiles from './SystemFiles';
+
+    export default {
+        components: {
+            SystemFiles
+        },
+        data() {
+            return {
+                currentType: '',
+                systemUserID: -1,
+                showFolders: false,
+                folderFiles: [],
+                showMenu: false,
+                x: 0,
+                y: 0,
+                showAddFolder: false,
+                folderName: ""
+            }
+        },
+        mounted() {
+            let self = this;
+            let encoded_details = jwt.decode(sessionStorage.accessToken);
+            self.systemUserID = encoded_details.USER_ID;
+        },
+        methods: {
+            folderClick() {
+                let self = this;
+
+                if (self.showFolders) {
+                    self.showFolders = false;
+                    self.folderFiles = [];
+                } else {
+                    self.getSystemFolderFiles(() => {
+                        self.showFolders = true;
+                    })
+                }
+            },
+            getSystemFolderFiles(callback) {
+                let self = this;
+
+                self.folderFiles = [];
+
+                Axios.get(process.env.VUE_APP_API + `SystemFolderFile?parentID=null`)
+                    .then(r => {
+                        r.data.systemFolderFileList.forEach(folderFile => {
+                            self.folderFiles.push(new treeItem(folderFile));
+                        });
+
+                        callback();
+                    })
+            },
+            onContextMenu(e, item) {
+                let self = this;
+                self.showMenu = false
+                self.x = e.clientX
+                self.y = e.clientY
+                self.$nextTick(() => {
+                    self.showMenu = true
+                })
+            },
+            addFolderFile(type) {
+                let self = this;
+
+                self.folderName = "";
+                self.currentType = type;
+
+                if (!self.showMyFiles)
+                    self.showMyFiles = true;
+
+                self.showAddFolder = true;
+
+                setTimeout(() => {
+                    self.$refs.folderName.focus();
+                }, 100);
+            },
+            submitFolderFile() {
+                let self = this;
+
+                let type = self.currentType == 'folder' ? 0 : 1;
+
+                let request = {
+                    parentID: null,
+                    name: self.folderName,
+                    type: type
+                }
+
+                self.createFolderFile(request, newFolderFile => {
+                    self.folderFiles.push(new treeItem(newFolderFile));
+                    self.showAddFolder = false;
+                })
+            },
+            createFolderFile(request, callback) {
+                let self = this;
+
+                Axios.post(process.env.VUE_APP_API + `SystemFolderFile`, request)
+                    .then(r => {
+                        callback(r.data.systemFolderFile);
+                    })
+            },
+        }
+    }
+
+    function treeItem(data) {
+        let self = this;
+
+        for (var prop in data) {
+            self[prop] = data[prop];
+        }
+
+        self.children = [];
+        self.showChildren = false;
+        self.showEdit = false;
+    }
+</script>
+
+<style>
+    .route-item {
+        cursor: pointer;
+        font-size: 12px;
+        display: flex;
+    }
+</style>
