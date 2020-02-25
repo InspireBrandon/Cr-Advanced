8a1a6fa92c86ba82cf9af70adbfd3cf4dccd0038
Brandon Lange <brandon@devinspire.co.za>
2020-02-25 10:15:30 +0200

    added grass

--- src/components/Main/FloorPlanning2D/3D/Index.vue
+++ src/components/Main/FloorPlanning2D/3D/Index.vue
@@ -79,8 +79,12 @@
                     // camera.attachControl(canvas, true);
 
                     // Add lights to the scene
-                    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-                    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+                    // var light1 = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(1, 1, 0), scene)
+                    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3((15 / pxlToMeterRatio),
+                        (25 / pxlToMeterRatio), 0), scene);
+                    //var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+                    // var shadowGenerator = new BABYLON.ShadowGenerator(1024, light2);
 
                     let myScale = 2;
                     let myScale2 = 2;
@@ -118,6 +122,7 @@
                     }, scene, false);
 
                     myGround.material = myMaterial;
+                    myGround.material.shadowColor = BABYLON.Color3.Red();
 
                     myGround.position.x = (50 / 2);
                     myGround.position.z = (-30 / 2);
@@ -126,100 +131,13 @@
                     myGround.checkCollisions = true;
 
                     items.forEach((element, idx) => {
-
-                        var material = new BABYLON.StandardMaterial("material", scene);
-                        material.diffuseTexture = new BABYLON.Texture("wood.jpg", scene);
-
-                        var box = BABYLON.MeshBuilder.CreateBox("box", {
-                            height: element.depth / pxlToMeterRatio,
-                            width: element.width / pxlToMeterRatio,
-                            depth: (element.height / 10) / pxlToMeterRatio
-                        }, scene);
-
-                        var base = BABYLON.MeshBuilder.CreateBox("box", {
-                            height: (element.depth / 10) / pxlToMeterRatio,
-                            width: element.width / pxlToMeterRatio,
-                            depth: element.height / pxlToMeterRatio
-                        }, scene);
-
-                        var shelf1 = BABYLON.MeshBuilder.CreateBox("box", {
-                            height: (element.depth / 50) / pxlToMeterRatio,
-                            width: element.width / pxlToMeterRatio,
-                            depth: (element.height) / pxlToMeterRatio
-                        }, scene);
-
-                        var shelf2 = BABYLON.MeshBuilder.CreateBox("box", {
-                            height: (element.depth / 50) / pxlToMeterRatio,
-                            width: element.width / pxlToMeterRatio,
-                            depth: (element.height) / pxlToMeterRatio
-                        }, scene);
-
-                        var shelf3 = BABYLON.MeshBuilder.CreateBox("box", {
-                            height: (element.depth / 50) / pxlToMeterRatio,
-                            width: element.width / pxlToMeterRatio,
-                            depth: (element.height) / pxlToMeterRatio
-                        }, scene);
-
-                        if (element.rotation == 90) {
-                            box.position.x = ((element.x + (0.5 * element.height)) / pxlToMeterRatio);
-                        } else if (element.rotation == 270) {
-                            box.position.x = ((element.x + (element.width - (0.5 * element.height))) /
-                                pxlToMeterRatio);
-                        } else {
-                            box.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                        if (element.name == "Gondola-Rect") {
+                            self.buildShelf(scene, element);
                         }
 
-                        base.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
-                        shelf1.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
-                        shelf2.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
-                        shelf3.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
-
-                        if (element.rotation == 90 || element.rotation == 270) {
-                            box.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-                        } else if (element.rotation == 180) {
-                            box.position.z = -((element.y + element.height) / pxlToMeterRatio);
-                        } else {
-                            box.position.z = -((element.y + (0.5 * (element.height / 10))) /
-                                pxlToMeterRatio);
+                        if (element.name == "wall") {
+                            self.buildWall(scene, element);
                         }
-
-                        base.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-
-                        shelf1.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-                        shelf2.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-                        shelf3.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-
-                        shelf1.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-                        shelf2.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-                        shelf3.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
-
-                        box.position.y = (element.depth / pxlToMeterRatio) / 2;
-                        base.position.y = ((element.depth / 10) / pxlToMeterRatio) / 2;
-                        shelf1.position.y = ((element.depth / 2) / pxlToMeterRatio) / 2;
-                        shelf2.position.y = ((element.depth / 1) / pxlToMeterRatio) / 2;
-                        shelf3.position.y = ((element.depth / 0.7) / pxlToMeterRatio) / 2;
-
-                        // add rotation
-                        let xPivot = -((element.width / 2) / pxlToMeterRatio);
-                        let zPivot = ((element.height / 2) / pxlToMeterRatio);
-
-                        box.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
-                        base.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
-                        shelf1.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
-                        shelf2.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
-                        shelf3.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
-
-                        box.rotation.y = degreesToRadians(element.rotation)
-                        base.rotation.y = degreesToRadians(element.rotation)
-                        shelf1.rotation.y = degreesToRadians(element.rotation)
-                        shelf2.rotation.y = degreesToRadians(element.rotation)
-                        shelf3.rotation.y = degreesToRadians(element.rotation)
-
-                        box.material = material;
-                        base.material = material;
-                        shelf1.material = material;
-                        shelf2.material = material;
-                        shelf3.material = material;
                     });
 
                     return scene;
@@ -247,6 +165,130 @@
                     .then(r => {
                         callback(r.data);
                     })
+            },
+            buildWall(scene, element) {
+                let self = this;
+
+                var material = new BABYLON.StandardMaterial("material", scene);
+                material.diffuseTexture = new BABYLON.Texture("wall-white.jpg", scene);
+
+                console.log("depth", element.depth)
+
+                var box = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: 80 / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: element.height / pxlToMeterRatio
+                }, scene);
+
+                box.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                box.position.z = -((element.y + (0.5 * (element.height / 10))) /
+                    pxlToMeterRatio);
+                box.position.y = (30 / pxlToMeterRatio) / 2;
+
+                // add rotation
+                let xPivot = -((element.width / 2) / pxlToMeterRatio);
+                let zPivot = ((element.height / 2) / pxlToMeterRatio);
+
+                box.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+                box.rotation.y = degreesToRadians(element.rotation)
+                box.material = material;
+            },
+            buildShelf(scene, element) {
+                let self = this;
+
+                var material = new BABYLON.StandardMaterial("material", scene);
+                material.diffuseTexture = new BABYLON.Texture("wood.jpg", scene);
+
+                var box = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: element.depth / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: (element.height / 10) / pxlToMeterRatio
+                }, scene);
+
+                var base = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: (element.depth / 10) / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: element.height / pxlToMeterRatio
+                }, scene);
+
+                var shelf1 = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: (element.depth / 50) / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: (element.height) / pxlToMeterRatio
+                }, scene);
+
+                var shelf2 = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: (element.depth / 50) / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: (element.height) / pxlToMeterRatio
+                }, scene);
+
+                var shelf3 = BABYLON.MeshBuilder.CreateBox("box", {
+                    height: (element.depth / 50) / pxlToMeterRatio,
+                    width: element.width / pxlToMeterRatio,
+                    depth: (element.height) / pxlToMeterRatio
+                }, scene);
+
+                if (element.rotation == 90) {
+                    box.position.x = ((element.x + (0.5 * element.height)) / pxlToMeterRatio);
+                } else if (element.rotation == 270) {
+                    box.position.x = ((element.x + (element.width - (0.5 * element.height))) /
+                        pxlToMeterRatio);
+                } else {
+                    box.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                }
+
+                base.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                shelf1.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                shelf2.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+                shelf3.position.x = ((element.x + (0.5 * element.width)) / pxlToMeterRatio);
+
+                if (element.rotation == 90 || element.rotation == 270) {
+                    box.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+                } else if (element.rotation == 180) {
+                    box.position.z = -((element.y + element.height) / pxlToMeterRatio);
+                } else {
+                    box.position.z = -((element.y + (0.5 * (element.height / 10))) /
+                        pxlToMeterRatio);
+                }
+
+                base.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+
+                shelf1.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+                shelf2.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+                shelf3.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+
+                shelf1.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+                shelf2.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+                shelf3.position.z = -((element.y + (0.5 * element.height)) / pxlToMeterRatio);
+
+                box.position.y = (element.depth / pxlToMeterRatio) / 2;
+                base.position.y = ((element.depth / 10) / pxlToMeterRatio) / 2;
+                shelf1.position.y = ((element.depth / 2) / pxlToMeterRatio) / 2;
+                shelf2.position.y = ((element.depth / 1) / pxlToMeterRatio) / 2;
+                shelf3.position.y = ((element.depth / 0.7) / pxlToMeterRatio) / 2;
+
+                // add rotation
+                let xPivot = -((element.width / 2) / pxlToMeterRatio);
+                let zPivot = ((element.height / 2) / pxlToMeterRatio);
+
+                box.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+                base.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+                shelf1.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+                shelf2.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+                shelf3.setPivotPoint(new BABYLON.Vector3(xPivot, 0, zPivot));
+
+                box.rotation.y = degreesToRadians(element.rotation)
+                base.rotation.y = degreesToRadians(element.rotation)
+                shelf1.rotation.y = degreesToRadians(element.rotation)
+                shelf2.rotation.y = degreesToRadians(element.rotation)
+                shelf3.rotation.y = degreesToRadians(element.rotation)
+
+                box.material = material;
+                base.material = material;
+                shelf1.material = material;
+                shelf2.material = material;
+                shelf3.material = material;
             }
         }
     }
